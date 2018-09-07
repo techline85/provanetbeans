131d21c24154c311375609dd44f7857e845e23a2
claudio-noto <claudio-noto@claudionoto-System-Product-Name>
2018-09-07 11:35:46 +0200

    creato branch1

--- test/index.php
+++ test/index.php
@@ -12,6 +12,8 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
+        
+          echo "prova effettuata su branch1"
         ?>
     </body>
 </html>
